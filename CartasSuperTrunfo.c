0ed77a954b23a5ee62bb768398f898d10dfd0e83
AlexsandroLek <alexnikeadd@gmail.com>
2026-02-22 11:06:30 -0300

    1
    Fix variable names and improve input/output for city cards.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,78 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  int populacao1, populacao2;
+  int pontosturisticos1, pontosturisticos2, carta1, carta2;
 
-  // Área para entrada de dados
+  float pib1, pib2, area1, area2;
+  float densidadepopulacional1, densidadepopulacional2;
+  float pib1, pib2;
+  float superpoder1, superpoder2;
 
+  char pais[50] = "Brasil";
+  char codigo1[20], codigo2[20];
+  char estado1[20], estado2[20];
+  char cidade1[20], cidade2[20];
+  
+  // Área para entrada de dados 
+  
+  // Carta 1
+  
+  printf("Escolha sua Carta: \n");
+  scanf("%s", &carta1);
+
+  printf("Codigo: \n");
+  scanf("%s", &codigo1);
+
+  printf("Estado: \n");
+  scanf("%s", &estado1);
+
+  printf("Cidade: \n");
+  scanf("%s", &ciadade1);
+
+  printf("Populacao: \n");
+  scanf("%f", &populacao1);
+
+  printf("Area: \n");
+  scanf("%f", &area1);
+
+  printf("Pib: \n");
+  scanf("%f", &pib1);
+
+  printf("Pontos Turisticos: \n");
+  scanf("%d", &pontosturisticos1);
+  
+    // Carta 2
+  
+  printf("Escolha sua Carta: \n");
+  scanf("%s", &carta2);
+
+  printf("Codigo: \n");
+  scanf("%s", &codigo2);
+
+  printf("Estado: \n");
+  scanf("%s", &estado2);
+
+  printf("Cidade: \n");
+  scanf("%s", &ciadade2);
+
+  printf("Populacao: \n");
+  scanf("%f", &populacao2);
+
+  printf("Area: \n");
+  scanf("%f", &area2);
+
+  printf("Pib: \n");
+  scanf("%f", &pib2);
+
+  printf("Pontos Turisticos: \n");
+  scanf("%d", &pontosturisticos2);
   // Área para exibição dos dados da cidade
+  
+  // Carta1
+  printf("\nCarta1: %d\nCodigo: %s\nEstado: %s\nCidade1: %sPopulação: %f\nArea: %f\nPontosTuristicos: %d\n",
+  Carta1, codigo1, estado1, ciadade1, populacao1, area1, pontosturisticos1);
 
+  
 return 0;
 } 
